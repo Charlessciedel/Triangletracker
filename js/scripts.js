ffb8e72597b84d896d8b8ce6e424bfb35144a008
George Charles <charlesschiedel@gmail.com>
2019-06-02 02:16:24 +0300

    I have added the condition for when there is no value input

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,6 @@ function triangleTracker() {
     var lengthOfSidex = parseInt(document.getElementById("Side x").value)
     var lengthOfSidey = parseInt(document.getElementById("Side y").value);
     var lengthOfSidez = parseInt(document.getElementById("Side z").value);
+
+    if (isNaN(lengthOfSidex)|| isNaN(lengthOfSidey)|| isNaN(lengthOfSidez)){
+        alert("please in put a number");
