6885348f69fe779886f29b45580ec47e2f02a464
George Charles <charlesschiedel@gmail.com>
2019-06-02 02:13:58 +0300

    I have defined the differnt variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,4 @@
-function triangleTracker()
+function triangleTracker() {
+    var lengthOfSidex = parseInt(document.getElementById("Side x").value)
+    var lengthOfSidey = parseInt(document.getElementById("Side y").value);
+    var lengthOfSidez = parseInt(document.getElementById("Side z").value);
